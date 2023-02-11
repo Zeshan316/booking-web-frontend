c8d908d039729a40ef8e0ecd3c4e941773a78e4e
Mahnoor <mahnoorahmed@dnamicro.com>
2023-02-11 20:45:23 +0500

    user avatar, logout modal and filters styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MDBContainer,
   MDBNavbar,
   MDBNavbarBrand,
   MDBBtn,
+  MDBCol,
 } from "mdb-react-ui-kit";
 import { useSelector } from "react-redux";
 import logo from "../../assets/bus-img.png";
@@ -11,13 +12,17 @@ import { RootState } from "../../../src/store";
 import { clearUserData } from "../../store/reducers/auth-reducer";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { SERVER_BASE_URL } from "../../common/constants";
+import DeleteModal from "../Toolbar/DeleteModal";
 
 const Header = (): JSX.Element => {
+  const [showConfirmBox, setShowConfirmBox] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const authData = useSelector((state: RootState) => state.auth);
-
+  const user = useSelector((state: RootState) => state.auth.user);
   function handleLogout() {
     dispatch(clearUserData());
     navigate("/login");
@@ -32,16 +37,40 @@ const Header = (): JSX.Element => {
             DNA Cab Service {authData?.user.role.name}
           </MDBNavbarBrand>
 
-          <MDBBtn
-            color="info"
-            type="button"
-            className="fw-bold fs-6  text-capitalize"
-            onClick={handleLogout}
-          >
-            Logout
-          </MDBBtn>
+          <MDBCol className="d-flex justify-content-end align-items-center">
+            <img
+              src={
+                `${SERVER_BASE_URL}${user?.profileImgUrl}` ||
+                `${SERVER_BASE_URL}static/images/defaultProfileImg.png`
+              }
+              className="rounded-circle"
+              height="50"
+              alt="Black and White Portrait of a Man"
+              loading="lazy"
+            />
+            <span className="fw-bold fs-6 ms-1 mt-4">
+              {user?.firstName} {user?.lastName}
+            </span>
+            <MDBBtn
+              color="info"
+              type="button"
+              className="fw-bold ms-3 text-capitalize"
+              onClick={() => setShowConfirmBox(true)}
+            >
+              Logout
+            </MDBBtn>
+          </MDBCol>
         </MDBContainer>
       </MDBNavbar>
+      {showConfirmBox && (
+        <DeleteModal
+          show={showConfirmBox}
+          message={"Are you sure you want to logout?"}
+          onDelete={handleLogout}
+          handleOnClose={() => setShowConfirmBox(false)}
+          setShow={() => setShowConfirmBox(!showConfirmBox)}
+        />
+      )}
     </>
   );
 };
--- src/components/Toolbar/DeleteModal.tsx
+++ src/components/Toolbar/DeleteModal.tsx
@@ -1,79 +1,74 @@
-import React, { useState } from 'react'
-import {
-	MDBBtn,
-	MDBModalBody,
-	MDBModalFooter,
-	MDBCol,
-} from 'mdb-react-ui-kit'
+import React, { useState } from "react";
+import { MDBBtn, MDBModalBody, MDBModalFooter, MDBCol } from "mdb-react-ui-kit";
 
-import ModalButton from '../Toolbar/ModalButton'
-import finalPropsSelectorFactory from 'react-redux/es/connect/selectorFactory'
+import ModalButton from "../Toolbar/ModalButton";
+import finalPropsSelectorFactory from "react-redux/es/connect/selectorFactory";
 
 interface ModalProps {
-	show: boolean
-	setShow: () => void
-	handleOnClose: () => void
-	onDelete: (e: any) => void
-	deleteData: any
-	message?: string
+  show: boolean;
+  setShow: () => void;
+  handleOnClose: () => void;
+  onDelete: (e: any) => void;
+  deleteData?: any;
+  message?: string;
 }
 function DeleteModal({
-	show,
-	setShow,
-	handleOnClose,
-	onDelete,
-	deleteData,
-	message,
+  show,
+  setShow,
+  handleOnClose,
+  onDelete,
+  deleteData,
+  message,
 }: ModalProps): JSX.Element {
-	const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-	const handleOpenModal = () => {
-		setIsOpen(!isOpen)
-	}
+  const handleOpenModal = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const handleDelete = () => {
-		onDelete(deleteData)
-		setShow()
-	}
+  const handleDelete = () => {
+    onDelete(deleteData);
+    setShow();
+  };
 
-	return (
-		<ModalButton
-			isOpen={show}
-			setIsOpen={handleOpenModal}
-			handleOnClose={setShow}
-			handleOpenModal={setShow}
-			iconname={'trash-alt'}
-			modalTitle='Delete'
-			modalBody={
-				<MDBModalBody>
-					<MDBCol className='mb-2 mx-1'>
-						<span className='fs-5 '>
-							{message
-								? message
-								: 'Are you sure you want to delete this record?'}
-						</span>
-					</MDBCol>
+  return (
+    <ModalButton
+      isOpen={show}
+      setIsOpen={handleOpenModal}
+      handleOnClose={setShow}
+      handleOpenModal={setShow}
+      iconname={"trash-alt"}
+      modalTitle="Delete"
+      modalBody={
+        <MDBModalBody>
+          <MDBCol className="mb-2 mx-1">
+            <span className="fs-5 ">
+              {message
+                ? message
+                : "Are you sure you want to delete this record?"}
+            </span>
+          </MDBCol>
 
-					<MDBModalFooter>
-						<MDBBtn
-							color='secondary'
-							className='text-capitalize fw-bold fs-6 '
-							onClick={setShow}
-						>
-							No
-						</MDBBtn>
-						<MDBBtn
-							color='danger'
-							className='text-capitalize fw-bold fs-6 '
-							onClick={handleDelete}
-						>
-							Yes
-						</MDBBtn>
-					</MDBModalFooter>
-				</MDBModalBody>
-			}
-		></ModalButton>
-	)
+          <MDBModalFooter>
+            <MDBBtn
+              color="secondary"
+              className="text-capitalize fw-bold fs-6 "
+              onClick={setShow}
+            >
+              No
+            </MDBBtn>
+            <MDBBtn
+              color="danger"
+              className="text-capitalize fw-bold fs-6 "
+              onClick={handleDelete}
+            >
+              Yes
+            </MDBBtn>
+          </MDBModalFooter>
+        </MDBModalBody>
+      }
+    ></ModalButton>
+  );
 }
 
-export default DeleteModal
+export default DeleteModal;
